8edaf5df1934f157644de36c80ad67afd4d5da92
paulnolan3 <144174178+paulnolan3@users.noreply.github.com>
2023-09-05 18:16:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 # Load the data
 data = pd.read_excel("PPD_Competencies.xlsx")
@@ -15,11 +16,27 @@ if selected_section == "All sections":
 else:
     filtered_data = data[data["PDD Section"] == selected_section]
 
+# Define Likert scale ordering and numerical mapping
+likert_order = ["Strongly Agree", "Agree", "Neutral", "Disagree", "Strongly Disagree"]
+likert_mapping = {
+    "Strongly Agree": 5,
+    "Agree": 4,
+    "Neutral": 3,
+    "Disagree": 2,
+    "Strongly Disagree": 1
+}
+
 # Visualize distribution of responses for each question
 for question in data.columns[2:-2]:  # Excluding the open-ended questions
-    value_counts = filtered_data[question].value_counts().reset_index()
+    value_counts = filtered_data[question].value_counts().reindex(likert_order).reset_index()
     value_counts.columns = ['Response', 'Count']
+    
+    # Calculate average response for the question
+    avg_response = filtered_data[question].map(likert_mapping).mean()
+    
+    # Plot
     fig = px.bar(value_counts, x='Response', y='Count', title=f"Responses for: {question}")
+    fig.add_trace(go.Scatter(x=[avg_response], y=[value_counts['Count'].max()], mode='markers+text', text=['Avg'], textposition='top center'))
     st.plotly_chart(fig)
 
 # Display random responses for the open-ended questions
