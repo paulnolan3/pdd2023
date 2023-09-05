8e3f0922d08a019816b873ad38954b502929bfa5
paulnolan3 <144174178+paulnolan3@users.noreply.github.com>
2023-09-05 18:38:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,12 +46,3 @@ elif selected_anchor == "Oral Briefing":
         value_counts.columns = ['Response', 'Count']
         fig = px.bar(value_counts, x='Response', y='Count', title=f"Responses for: {question}")
         st.plotly_chart(fig)
-
-# Display random responses for the open-ended questions
-for question in data.columns[-2:]:
-    st.write(f"Random Responses for: {question}")
-    available_responses = filtered_data[question].dropna()
-    sample_size = min(len(available_responses), 5)
-    sample_responses = available_responses.sample(sample_size)
-    for idx, response in sample_responses.iteritems():
-        st.write(f"- {response}")
