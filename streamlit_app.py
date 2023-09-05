fd7e28512b1a50d41c38193796d873ea4d12954b
paulnolan3 <144174178+paulnolan3@users.noreply.github.com>
2023-09-05 18:30:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,23 @@ else:
 # Define Likert scale ordering
 likert_order = ["Strongly Agree", "Agree", "Neutral", "Disagree", "Strongly Disagree"]
 
+def wrap_title(title, max_length=30):
+    """Wrap title if it exceeds max_length."""
+    if len(title) > max_length:
+        # Split title roughly in the middle
+        midpoint = len(title) // 2
+        # Find the nearest space to the midpoint
+        break_point = title.rfind(' ', 0, midpoint)
+        if break_point != -1:
+            return title[:break_point] + "<br>" + title[break_point+1:]
+    return title
+
 # Visualize distribution of responses for the "Policy Design Framework/Process" section
 st.header("Policy Design Framework/Process")
 for question in data.columns[2:12]:  # First 10 questions
     value_counts = filtered_data[question].value_counts().reindex(likert_order).reset_index()
     value_counts.columns = ['Response', 'Count']
-    fig = px.bar(value_counts, x='Response', y='Count', title=f"Responses for: {question}")
+    fig = px.bar(value_counts, x='Response', y='Count', title=wrap_title(f"Responses for: {question}"))
     st.plotly_chart(fig)
 
 # Visualize distribution of responses for the "Memo Writing" section
@@ -31,7 +42,7 @@ st.header("Memo Writing")
 for question in data.columns[12:15]:  # Next 3 questions
     value_counts = filtered_data[question].value_counts().reindex(likert_order).reset_index()
     value_counts.columns = ['Response', 'Count']
-    fig = px.bar(value_counts, x='Response', y='Count', title=f"Responses for: {question}")
+    fig = px.bar(value_counts, x='Response', y='Count', title=wrap_title(f"Responses for: {question}"))
     st.plotly_chart(fig)
 
 # Visualize distribution of responses for the "Oral Briefing" section
@@ -39,7 +50,7 @@ st.header("Oral Briefing")
 for question in data.columns[15:18]:  # Final 3 questions
     value_counts = filtered_data[question].value_counts().reindex(likert_order).reset_index()
     value_counts.columns = ['Response', 'Count']
-    fig = px.bar(value_counts, x='Response', y='Count', title=f"Responses for: {question}")
+    fig = px.bar(value_counts, x='Response', y='Count', title=wrap_title(f"Responses for: {question}"))
     st.plotly_chart(fig)
 
 # Display random responses for the open-ended questions
