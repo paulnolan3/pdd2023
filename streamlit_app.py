59eb15970d68b30ea226180245c7aa22a4d53349
paulnolan3 <144174178+paulnolan3@users.noreply.github.com>
2023-09-05 23:11:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,32 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
+import io
+
+# Display logo at the top of the sidebar
+st.sidebar.image("logo.png", use_column_width=True)
+
+# Load the data
+data = pd.read_excel("PPD_Competencies.xlsx")
+
+# Sidebar for PDD Section filter using radio buttons
+sections = ["All sections"] + list(data["PDD Section"].unique())
+selected_section = st.sidebar.radio("Select PDD Section", sections)
+
+# Sidebar buttons for navigating to sections
+selected_anchor = st.sidebar.radio("Navigate to", ["Policy Design Framework/Process", "Memo Writing", "Oral Briefing"])
+
+# Define Likert scale ordering and corresponding colors
+likert_order = ["Strongly Agree", "Agree", "Neutral", "Disagree", "Strongly Disagree"]
+colors = {'Strongly Agree': 'green', 'Agree': 'lightgreen', 'Neutral': 'gray', 'Disagree': 'lightcoral', 'Strongly Disagree': 'red'}
+
+# Filter data according to the selected section
+if selected_section == "All sections":
+    filtered_data = data
+else:
+    filtered_data = data[data["PDD Section"] == selected_section]
+
 def break_title(title, max_length=30):
     """
     Break a title into lines, ensuring that words are not split and each line does not exceed max_length.
@@ -30,3 +59,19 @@ def plot_chart(question):
     
     st.plotly_chart(fig)
     st.download_button(label="Download PNG", data=buf, file_name=f"{question}.png", mime="image/png")
+
+# Show content based on the selected anchor
+if selected_anchor == "Policy Design Framework/Process":
+    st.header("Policy Design Framework/Process")
+    for question in data.columns[2:12]:  # First 10 questions
+        plot_chart(question)
+
+elif selected_anchor == "Memo Writing":
+    st.header("Memo Writing")
+    for question in data.columns[12:15]:  # Next 3 questions
+        plot_chart(question)
+
+elif selected_anchor == "Oral Briefing":
+    st.header("Oral Briefing")
+    for question in data.columns[15:18]:  # Final 3 questions
+        plot_chart(question)
