b7a44e655b214be7ee92d33e6b695c0754cc9045
paulnolan3 <144174178+paulnolan3@users.noreply.github.com>
2023-09-05 22:17:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ data = pd.read_excel("PPD_Competencies.xlsx")
 sections = ["All sections"] + list(data["PDD Section"].unique())
 selected_section = st.sidebar.radio("Select PDD Section", sections)
 
-# Sidebar buttons for navigating to sections
-selected_anchor = st.sidebar.radio("Navigate to", ["Policy Design Framework/Process", "Memo Writing", "Oral Briefing"])
+# Sidebar buttons for navigating to sections or pages
+selected_anchor = st.sidebar.radio("Navigate to", ["Policy Design Framework/Process", "Memo Writing", "Oral Briefing", "Summary Statistics"])
 
 # Define Likert scale ordering and corresponding colors
 likert_order = ["Strongly Agree", "Agree", "Neutral", "Disagree", "Strongly Disagree"]
@@ -23,7 +23,7 @@ if selected_section == "All sections":
 else:
     filtered_data = data[data["PDD Section"] == selected_section]
 
-# Only show content for the selected anchor
+# Show content for the selected anchor or page
 if selected_anchor == "Policy Design Framework/Process":
     st.header("Policy Design Framework/Process")
     for question in data.columns[2:12]:  # First 10 questions
@@ -47,3 +47,15 @@ elif selected_anchor == "Oral Briefing":
         value_counts.columns = ['Response', 'Count']
         fig = px.bar(value_counts, x='Response', y='Count', title=f"Responses for: {question}", color='Response', color_discrete_map=colors)
         st.plotly_chart(fig)
+
+elif selected_anchor == "Summary Statistics":
+    st.header("Summary Statistics")
+
+    # Calculate and display the average scores for each section
+    policy_design_avg = filtered_data[data.columns[2:12]].mean(axis=1).mean()
+    memo_writing_avg = filtered_data[data.columns[12:15]].mean(axis=1).mean()
+    oral_briefing_avg = filtered_data[data.columns[15:18]].mean(axis=1).mean()
+
+    st.write(f"Average Score for Policy Design Framework/Process: {policy_design_avg:.2f}")
+    st.write(f"Average Score for Memo Writing: {memo_writing_avg:.2f}")
+    st.write(f"Average Score for Oral Briefing: {oral_briefing_avg:.2f}")
